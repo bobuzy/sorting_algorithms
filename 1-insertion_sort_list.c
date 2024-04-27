1efea5c454281444b9043adedf86e337f9287224
Benedict <bobuzytech@gmail.com>
2024-04-27 08:44:10 -0700

    Sort a doubly linked list of integers

---
+++ 1-insertion_sort_list.c
@@ -0,0 +1,68 @@
+#include "sort.h"
+
+/**
+ * swap_func - Swap position of two nodes
+ * @x: First node
+ * @y: Second node
+
+ * Return: nothing
+*/
+
+void swap(listint_t *pre, listint_t *curr)
+{
+	if (pre->prev)
+	{
+		pre->prev->next = curr;
+	}
+	if (curr->next)
+	{
+		curr->next->prev = pre;
+	}
+
+	pre->next = curr->next;
+	curr->prev = pre->prev;
+	pre->prev = curr;
+	curr->next = pre;
+}
+
+/**
+ * insertion_sort_list - Sort doubly linked list of int in ascending order
+ * @list: list to be sorted
+
+ * Return: nothing
+*/
+
+void insertion_sort_list(listint_t **list)
+{
+	listint_t *curr, *temp;
+
+	if (list == NULL || *list == NULL || (*list)->next == NULL)
+	{
+        return;
+    }
+	
+	curr = (*list)->next;
+	
+	while (curr != NULL)
+	{
+		temp = curr;
+
+		while(temp != NULL && temp->prev != NULL)
+		{
+			if (temp->prev->n > temp->n)
+			{
+				swap(temp->prev, temp);
+				if (!temp->prev)
+				{
+					*list = temp;
+				}
+				print_list(*list);
+			}
+			else
+			{
+				temp = temp->prev;
+			}
+		}
+		curr = curr->next;
+	}
+}
